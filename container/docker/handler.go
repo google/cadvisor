875146e396db62748fef56d5a5b337792057cf3a
Nan Monnand Deng <monnand@gmail.com>
2014-06-27 13:03:05 -0400

    calculate working set based on #. pages in active LRU.

--- container/docker/handler.go
+++ container/docker/handler.go
@@ -209,6 +209,12 @@ func libcontainerToContainerStats(s *cgroups.Stats, mi *info.MachineInfo) *info.
 		ret.Memory.ContainerData.Pgmajfault = v
 		ret.Memory.HierarchicalData.Pgmajfault = v
 	}
+	if v, ok := s.MemoryStats.Stats["active_anon"]; ok {
+		ret.Memory.WorkingSet = v
+		if v, ok := s.MemoryStats.Stats["active_file"]; ok {
+			ret.Memory.WorkingSet += v
+		}
+	}
 	return ret
 }
 
