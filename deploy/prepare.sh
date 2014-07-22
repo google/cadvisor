07b9ed32443ac0ba370bb0e09bc665c68e9f40b8
Victor Marmol <vmarmol@google.com>
2014-07-22 14:09:42 -0700

    Remove lmctfy driver in favor of the raw driver

--- deploy/prepare.sh
+++ deploy/prepare.sh
@@ -3,9 +3,5 @@
 set -e
 set -x
 
-# Download lmctfy.
-wget http://storage.googleapis.com/cadvisor-bin/lmctfy/lmctfy
-chmod +x lmctfy
-
 # Statically build cAdvisor from source and stage it.
 go build --ldflags '-extldflags "-static"' github.com/google/cadvisor
