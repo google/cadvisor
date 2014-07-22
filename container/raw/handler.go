e951d03be6ce37462db0efd1c61f1aadc3c0630d
Victor Marmol <vmarmol@google.com>
2014-07-22 08:44:32 -0700

    Implement GeSpec() in the simple raw driver.

--- container/raw/handler.go
+++ container/raw/handler.go
@@ -16,23 +16,30 @@ package raw
 
 import (
 	"io/ioutil"
+	"log"
+	"math"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/docker/libcontainer/cgroups"
 	"github.com/google/cadvisor/container"
 	"github.com/google/cadvisor/container/libcontainer"
 	"github.com/google/cadvisor/info"
+	"github.com/google/cadvisor/utils"
 )
 
 type rawContainerHandler struct {
-	name             string
-	cgroupSubsystems *cgroupSubsystems
+	name               string
+	cgroupSubsystems   *cgroupSubsystems
+	machineInfoFactory info.MachineInfoFactory
 }
 
-func newRawContainerHandler(name string, cgroupSubsystems *cgroupSubsystems) (container.ContainerHandler, error) {
+func newRawContainerHandler(name string, cgroupSubsystems *cgroupSubsystems, machineInfoFactory info.MachineInfoFactory) (container.ContainerHandler, error) {
 	return &rawContainerHandler{
-		name:             name,
-		cgroupSubsystems: cgroupSubsystems,
+		name:               name,
+		cgroupSubsystems:   cgroupSubsystems,
+		machineInfoFactory: machineInfoFactory,
 	}, nil
 }
 
@@ -43,13 +50,69 @@ func (self *rawContainerHandler) ContainerReference() (info.ContainerReference, 
 	}, nil
 }
 
-func (self *rawContainerHandler) GetSpec() (*info.ContainerSpec, error) {
-	ret := new(info.ContainerSpec)
-	ret.Cpu = new(info.CpuSpec)
-	ret.Memory = new(info.MemorySpec)
+func readInt64(path string, file string) uint64 {
+	cgroupFile := filepath.Join(path, file)
 
-	// TODO(vmarmol): Implement
-	return ret, nil
+	// Ignore non-existent files
+	if !utils.FileExists(cgroupFile) {
+		return 0
+	}
+
+	// Read
+	out, err := ioutil.ReadFile(cgroupFile)
+	if err != nil {
+		log.Printf("raw driver: Failed to read %q: %s", cgroupFile, err)
+		return 0
+	}
+	val, err := strconv.ParseUint(strings.TrimSpace(string(out)), 10, 64)
+	if err != nil {
+		log.Printf("raw driver: Failed to parse in %q from file %q: %s", string(out), cgroupFile, err)
+		return 0
+	}
+
+	return val
+}
+
+func (self *rawContainerHandler) GetSpec() (*info.ContainerSpec, error) {
+	spec := new(info.ContainerSpec)
+
+	// The raw driver assumes unified hierarchy containers.
+
+	// CPU.
+	cpuRoot, ok := self.cgroupSubsystems.mountPoints["cpu"]
+	if ok {
+		cpuRoot = filepath.Join(cpuRoot, self.name)
+		if utils.FileExists(cpuRoot) {
+			// Get machine info.
+			mi, err := self.machineInfoFactory.GetMachineInfo()
+			if err != nil {
+				return nil, err
+			}
+
+			spec.Cpu = new(info.CpuSpec)
+			spec.Cpu.Limit = readInt64(cpuRoot, "cpu.shares")
+
+			// TODO(vmarmol): Get CPUs from config.Cgroups.CpusetCpus
+			n := (mi.NumCores + 63) / 64
+			spec.Cpu.Mask.Data = make([]uint64, n)
+			for i := 0; i < n; i++ {
+				spec.Cpu.Mask.Data[i] = math.MaxUint64
+			}
+		}
+	}
+
+	// Memory.
+	memoryRoot, ok := self.cgroupSubsystems.mountPoints["memory"]
+	if ok {
+		memoryRoot = filepath.Join(memoryRoot, self.name)
+		if utils.FileExists(memoryRoot) {
+			spec.Memory = new(info.MemorySpec)
+			spec.Memory.Limit = readInt64(memoryRoot, "memory.limit_in_bytes")
+			spec.Memory.SwapLimit = readInt64(memoryRoot, "memory.limit_in_bytes")
+		}
+	}
+
+	return spec, nil
 }
 
 func (self *rawContainerHandler) GetStats() (stats *info.ContainerStats, err error) {
