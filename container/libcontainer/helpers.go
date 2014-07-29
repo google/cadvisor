fb5074a2f1bca2530addcd305fa99eefaecf131c
Nan Monnand Deng <monnand@gmail.com>
2014-07-29 01:52:59 -0400

    unit test for SplitName()

--- container/libcontainer/helpers.go
+++ container/libcontainer/helpers.go
@@ -1,8 +1,21 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package libcontainer
 
 import (
 	"bufio"
-	"os"
 	"path"
 	"path/filepath"
 	"strings"
@@ -10,8 +23,9 @@ import (
 
 	"github.com/docker/libcontainer"
 	"github.com/docker/libcontainer/cgroups"
-	"github.com/docker/libcontainer/cgroups/fs"
+	cgroupfs "github.com/docker/libcontainer/cgroups/fs"
 	"github.com/google/cadvisor/info"
+	"github.com/google/cadvisor/utils/fs"
 )
 
 // Get stats of the specified container
@@ -25,7 +39,7 @@ func GetStats(config *libcontainer.Config, state *libcontainer.State) (*info.Con
 }
 
 func GetStatsCgroupOnly(cgroup *cgroups.Cgroup) (*info.ContainerStats, error) {
-	s, err := fs.GetStats(cgroup)
+	s, err := cgroupfs.GetStats(cgroup)
 	if err != nil {
 		return nil, err
 	}
@@ -78,7 +92,7 @@ func toContainerStats(libcontainerStats *libcontainer.ContainerStats) *info.Cont
 // Given a container name, returns the parent and name of the container to be fed to libcontainer.
 func SplitName(containerName string) (string, string, error) {
 	parent, id := path.Split(containerName)
-	cgroupSelf, err := os.Open("/proc/1/cgroup")
+	cgroupSelf, err := fs.Open("/proc/1/cgroup")
 	if err != nil {
 		return "", "", err
 	}
