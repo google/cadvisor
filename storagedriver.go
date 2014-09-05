f32457747db26732c5983422ea1762788af1ba66
Nan Monnand Deng <monnand@gmail.com>
2014-09-05 17:42:58 -0400

    glog.Infof()

--- storagedriver.go
+++ storagedriver.go
@@ -87,7 +87,7 @@ func NewStorageDriver(driverName string) (*memory.InMemoryStorage, error) {
 	if err != nil {
 		return nil, err
 	}
-	glog.V(2).Infof("Caching %d recent stats in memory; using %v storage driver\n", samplesToCache, driverName)
+	glog.Infof("Caching %d recent stats in memory; using %v storage driver\n", samplesToCache, driverName)
 	storageDriver = memory.New(samplesToCache, samplesToCache, backendStorage)
 	return storageDriver, nil
 }
