833feae28b98d9a7b087ec166d560b2cbeefe20f
Satnam Singh <satnam@google.com>
2014-09-24 10:29:04 -0700

    Fix assignment error in haste.

--- manager/manager.go
+++ manager/manager.go
@@ -423,7 +423,7 @@ func (self *manager) watchForNewContainers(quit chan error) error {
 	}
 
 	// There is a race between starting the watch and new container creation so we do a detection before we read new containers.
-	err := self.detectSubcontainers("/")
+	err = self.detectSubcontainers("/")
 	if err != nil {
 		return err
 	}
