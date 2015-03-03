c1959cebeec1e5f7b6aa36cb5873829a475fed70
Victor Marmol <vmarmol@google.com>
2015-03-03 10:15:42 -0800

    Add ManagerMock.

---
+++ manager/manager_mock.go
@@ -0,0 +1,85 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package manager
+
+import (
+	"github.com/google/cadvisor/events"
+	"github.com/google/cadvisor/info"
+	"github.com/stretchr/testify/mock"
+)
+
+type ManagerMock struct {
+	mock.Mock
+}
+
+func (c *ManagerMock) Start() error {
+	args := c.Called()
+	return args.Error(0)
+}
+
+func (c *ManagerMock) Stop() error {
+	args := c.Called()
+	return args.Error(0)
+}
+
+func (c *ManagerMock) GetContainerInfo(name string, query *info.ContainerInfoRequest) (*info.ContainerInfo, error) {
+	args := c.Called(name, query)
+	return args.Get(0).(*info.ContainerInfo), args.Error(1)
+}
+
+func (c *ManagerMock) SubcontainersInfo(containerName string, query *info.ContainerInfoRequest) ([]*info.ContainerInfo, error) {
+	args := c.Called(containerName, query)
+	return args.Get(0).([]*info.ContainerInfo), args.Error(1)
+}
+
+func (c *ManagerMock) AllDockerContainers(query *info.ContainerInfoRequest) (map[string]info.ContainerInfo, error) {
+	args := c.Called(query)
+	return args.Get(0).(map[string]info.ContainerInfo), args.Error(1)
+}
+
+func (c *ManagerMock) DockerContainer(name string, query *info.ContainerInfoRequest) (info.ContainerInfo, error) {
+	args := c.Called(name, query)
+	return args.Get(0).(info.ContainerInfo), args.Error(1)
+}
+
+func (c *ManagerMock) GetContainerSpec(containerName string) (info.ContainerSpec, error) {
+	args := c.Called(containerName)
+	return args.Get(0).(info.ContainerSpec), args.Error(1)
+}
+
+func (c *ManagerMock) GetContainerDerivedStats(containerName string) (info.DerivedStats, error) {
+	args := c.Called(containerName)
+	return args.Get(0).(info.DerivedStats), args.Error(1)
+}
+
+func (c *ManagerMock) WatchForEvents(queryuest *events.Request, passedChannel chan *events.Event) error {
+	args := c.Called(queryuest, passedChannel)
+	return args.Error(0)
+}
+
+func (c *ManagerMock) GetPastEvents(queryuest *events.Request) (events.EventSlice, error) {
+	args := c.Called(queryuest)
+	return args.Get(0).(events.EventSlice), args.Error(1)
+}
+
+func (c *ManagerMock) GetMachineInfo() (*info.MachineInfo, error) {
+	args := c.Called()
+	return args.Get(0).(*info.MachineInfo), args.Error(1)
+}
+
+func (c *ManagerMock) GetVersionInfo() (*info.VersionInfo, error) {
+	args := c.Called()
+	return args.Get(0).(*info.VersionInfo), args.Error(1)
+}
