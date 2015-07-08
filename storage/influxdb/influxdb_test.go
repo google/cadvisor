e4fa01857cce4f3a71b13732421f6ebbe90cfc4e
Matthias Nüßler <m.nuessler@web.de>
2015-07-08 09:46:54 +0200

    Upgrade InfluxDB storage to InfluxDB 0.9
    - Fix #743
    - Rewrite InfluxDB storage for new InfluxDB API data structures.
    - Store each measurement separately instead of storing all measurements
      in a single big "table" with many columns/fields.
    - Use tags add metadata to points, such as the container name. Tags are
      a new feature in InfluxDB 0.9.

--- storage/influxdb/influxdb_test.go
+++ storage/influxdb/influxdb_test.go
@@ -19,6 +19,8 @@ package influxdb
 
 import (
 	"fmt"
+	"math/rand"
+	"net/url"
 	"reflect"
 	"testing"
 	"time"
@@ -28,6 +30,8 @@ import (
 	"github.com/google/cadvisor/storage/test"
 
 	influxdb "github.com/influxdb/influxdb/client"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // The duration in seconds for which stats will be buffered in the influxdb driver.
@@ -40,27 +44,12 @@ type influxDbTestStorageDriver struct {
 }
 
 func (self *influxDbTestStorageDriver) readyToFlush() bool {
-	if self.count >= self.buffer {
-		return true
-	}
-	return false
+	return self.count >= self.buffer
 }
 
 func (self *influxDbTestStorageDriver) AddStats(ref info.ContainerReference, stats *info.ContainerStats) error {
 	self.count++
 	return self.base.AddStats(ref, stats)
-}
-
-func (self *influxDbTestStorageDriver) RecentStats(containerName string, numStats int) ([]*info.ContainerStats, error) {
-	return nil, nil
-}
-
-func (self *influxDbTestStorageDriver) Percentiles(containerName string, cpuUsagePercentiles []int, memUsagePercentiles []int) (*info.ContainerStatsPercentiles, error) {
-	return self.base.Percentiles(containerName, cpuUsagePercentiles, memUsagePercentiles)
-}
-
-func (self *influxDbTestStorageDriver) Samples(containerName string, numSamples int) ([]*info.ContainerStatsSample, error) {
-	return self.base.Samples(containerName, numSamples)
 }
 
 func (self *influxDbTestStorageDriver) Close() error {
@@ -74,6 +63,28 @@ func (self *influxDbTestStorageDriver) StatsEq(a, b *info.ContainerStats) bool {
 	// Check only the stats populated in influxdb.
 	if a.Cpu.Usage.Total != b.Cpu.Usage.Total {
 		return false
+	}
+	if a.Cpu.Usage.System != b.Cpu.Usage.System {
+		return false
+	}
+	if a.Cpu.Usage.User != b.Cpu.Usage.User {
+		return false
+	}
+
+	// TODO simpler way to check if arrays are equal?
+	if a.Cpu.Usage.PerCpu == nil && b.Cpu.Usage.PerCpu != nil {
+		return false
+	}
+	if a.Cpu.Usage.PerCpu != nil && b.Cpu.Usage.PerCpu == nil {
+		return false
+	}
+	if len(a.Cpu.Usage.PerCpu) != len(b.Cpu.Usage.PerCpu) {
+		return false
+	}
+	for i, usage := range a.Cpu.Usage.PerCpu {
+		if usage != b.Cpu.Usage.PerCpu[i] {
+			return false
+		}
 	}
 
 	if a.Memory.Usage != b.Memory.Usage {
@@ -96,73 +107,56 @@ func (self *influxDbTestStorageDriver) StatsEq(a, b *info.ContainerStats) bool {
 
 func runStorageTest(f func(test.TestStorageDriver, *testing.T), t *testing.T, bufferCount int) {
 	machineName := "machineA"
-	tablename := "t"
-	database := "cadvisor"
+	database := "cadvisor_test"
 	username := "root"
 	password := "root"
 	hostname := "localhost:8086"
-	percentilesDuration := 10 * time.Minute
-	rootConfig := &influxdb.ClientConfig{
-		Host:     hostname,
+	//percentilesDuration := 10 * time.Minute
+
+	config := influxdb.Config{
+		URL:      url.URL{Scheme: "http", Host: hostname},
 		Username: username,
 		Password: password,
-		IsSecure: false,
-	}
-	rootClient, err := influxdb.NewClient(rootConfig)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// create the data base first.
-	rootClient.CreateDatabase(database)
-	config := &influxdb.ClientConfig{
-		Host:     hostname,
-		Username: username,
-		Password: password,
-		Database: database,
-		IsSecure: false,
 	}
 	client, err := influxdb.NewClient(config)
 	if err != nil {
 		t.Fatal(err)
 	}
-	client.DisableCompression()
-	deleteAll := fmt.Sprintf("drop series %v", tablename)
-	_, err = client.Query(deleteAll)
-	if err != nil {
+
+	// Re-create the database first.
+	if err := prepareDatabase(client, database); err != nil {
 		t.Fatal(err)
 	}
-	// delete all data by the end of the call
-	defer client.Query(deleteAll)
+
+	// Delete all data by the end of the call.
+	//defer client.Query(influxdb.Query{Command: fmt.Sprintf("drop database \"%v\"", database)})
 
 	driver, err := New(machineName,
-		tablename,
 		database,
 		username,
 		password,
 		hostname,
 		false,
-		time.Duration(bufferCount),
-		percentilesDuration)
+		time.Duration(bufferCount))
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer driver.Close()
 	testDriver := &influxDbTestStorageDriver{buffer: bufferCount}
 	driver.OverrideReadyToFlush(testDriver.readyToFlush)
 	testDriver.base = driver
 
-	// generate another container's data on same machine.
+	// Generate another container's data on same machine.
 	test.StorageDriverFillRandomStatsFunc("containerOnSameMachine", 100, testDriver, t)
 
-	// generate another container's data on another machine.
+	// Generate another container's data on another machine.
 	driverForAnotherMachine, err := New("machineB",
-		tablename,
 		database,
 		username,
 		password,
 		hostname,
 		false,
-		time.Duration(bufferCount),
-		percentilesDuration)
+		time.Duration(bufferCount))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -175,14 +169,138 @@ func runStorageTest(f func(test.TestStorageDriver, *testing.T), t *testing.T, bu
 	f(testDriver, t)
 }
 
-func TestRetrievePartialRecentStats(t *testing.T) {
-	runStorageTest(test.StorageDriverTestRetrievePartialRecentStats, t, 20)
+func prepareDatabase(client *influxdb.Client, database string) error {
+	dropDbQuery := influxdb.Query{
+		Command: fmt.Sprintf("drop database \"%v\"", database),
+	}
+	createDbQuery := influxdb.Query{
+		Command: fmt.Sprintf("create database \"%v\"", database),
+	}
+	// A default retention policy must always be present.
+	// Depending on the InfluxDB configuration it may be created automatically with the database or not.
+	// TODO create ret. policy only if not present
+	createPolicyQuery := influxdb.Query{
+		Command: fmt.Sprintf("create retention policy \"default\" on \"%v\" duration 1h replication 1 default", database),
+	}
+	_, err := client.Query(dropDbQuery)
+	if err != nil {
+		return err
+	}
+	_, err = client.Query(createDbQuery)
+	if err != nil {
+		return err
+	}
+	_, err = client.Query(createPolicyQuery)
+	return err
 }
 
-func TestRetrieveAllRecentStats(t *testing.T) {
-	runStorageTest(test.StorageDriverTestRetrieveAllRecentStats, t, 10)
+func TestContainerFileSystemStatsToPoints(t *testing.T) {
+	assert := assert.New(t)
+
+	machineName := "testMachine"
+	database := "cadvisor_test"
+	username := "root"
+	password := "root"
+	influxdbHost := "localhost:8086"
+
+	storage, err := New(machineName,
+		database,
+		username,
+		password,
+		influxdbHost,
+		false, 2*time.Minute)
+	assert.Nil(err)
+
+	ref := info.ContainerReference{
+		Name: "containerName",
+	}
+	stats := &info.ContainerStats{}
+	points := storage.containerFilesystemStatsToPoints(ref, stats)
+
+	// stats.Filesystem is always nil, not sure why
+	assert.Nil(points)
 }
 
-func TestNoRecentStats(t *testing.T) {
-	runStorageTest(test.StorageDriverTestNoRecentStats, t, kCacheDuration)
+func TestContainerStatsToPoints(t *testing.T) {
+	// Given
+	storage, err := createTestStorage()
+	require.Nil(t, err)
+	require.NotNil(t, storage)
+
+	ref, stats := createTestStats()
+	require.Nil(t, err)
+	require.NotNil(t, stats)
+
+	// When
+	points := storage.containerStatsToPoints(*ref, stats)
+
+	// Then
+	assert.NotEmpty(t, points)
+	assert.Len(t, points, 10+len(stats.Cpu.Usage.PerCpu))
+
+	assertContainsPointWithValue(t, points, serCpuUsageTotal, stats.Cpu.Usage.Total)
+	assertContainsPointWithValue(t, points, serCpuUsageSystem, stats.Cpu.Usage.System)
+	assertContainsPointWithValue(t, points, serCpuUsageUser, stats.Cpu.Usage.User)
+	assertContainsPointWithValue(t, points, serMemoryUsage, stats.Memory.Usage)
+	assertContainsPointWithValue(t, points, serLoadAverage, stats.Cpu.LoadAverage)
+	assertContainsPointWithValue(t, points, serMemoryWorkingSet, stats.Memory.WorkingSet)
+	assertContainsPointWithValue(t, points, serRxBytes, stats.Network.RxBytes)
+	assertContainsPointWithValue(t, points, serRxErrors, stats.Network.RxErrors)
+	assertContainsPointWithValue(t, points, serTxBytes, stats.Network.TxBytes)
+	assertContainsPointWithValue(t, points, serTxBytes, stats.Network.TxErrors)
+
+	for _, cpu_usage := range stats.Cpu.Usage.PerCpu {
+		assertContainsPointWithValue(t, points, serCpuUsagePerCpu, cpu_usage)
+	}
+}
+
+func assertContainsPointWithValue(t *testing.T, points []*influxdb.Point, name string, value interface{}) bool {
+	found := false
+	for _, point := range points {
+		if point.Measurement == name && point.Fields[fieldValue] == toSignedIfUnsigned(value) {
+			found = true
+			break
+		}
+	}
+	return assert.True(t, found, "no point found with name='%v' and value=%v", name, value)
+}
+
+func createTestStorage() (*influxdbStorage, error) {
+	machineName := "testMachine"
+	database := "cadvisor_test"
+	username := "root"
+	password := "root"
+	influxdbHost := "localhost:8086"
+
+	storage, err := New(machineName,
+		database,
+		username,
+		password,
+		influxdbHost,
+		false, 2*time.Minute)
+
+	return storage, err
+}
+
+func createTestStats() (*info.ContainerReference, *info.ContainerStats) {
+	ref := &info.ContainerReference{
+		Name:    "testContainername",
+		Aliases: []string{"testContainerAlias1", "testContainerAlias2"},
+	}
+
+	cpuUsage := info.CpuUsage{
+		Total:  uint64(rand.Intn(10000)),
+		PerCpu: []uint64{uint64(rand.Intn(1000)), uint64(rand.Intn(1000)), uint64(rand.Intn(1000))},
+		User:   uint64(rand.Intn(10000)),
+		System: uint64(rand.Intn(10000)),
+	}
+
+	stats := &info.ContainerStats{
+		Timestamp: time.Now(),
+		Cpu: info.CpuStats{
+			Usage:       cpuUsage,
+			LoadAverage: int32(rand.Intn(1000)),
+		},
+	}
+	return ref, stats
 }
