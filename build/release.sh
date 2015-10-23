aa6f80814bc6fdb43a0ed12719658225420ffb7d
Jimmi Dyson <jimmidyson@gmail.com>
2015-10-23 13:22:17 +0100

    Fix up release process

--- build/release.sh
+++ build/release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 version=$( cat version/VERSION )
+branch=$( git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown' )
 
 rm -rf release && mkdir release
 cp cadvisor release/cadvisor
 go get -u github.com/progrium/gh-release
-gh-release create google/cadvisor ${version}
+gh-release create google/cadvisor ${version} ${branch} ${version}
