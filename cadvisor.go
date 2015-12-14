f6a2def861c61cfaeeb7afa56edb22189bfd8cf9
Phillip Wittrock <pwittroc@google.com>
2015-12-14 11:41:02 -0800

    Do not merge.  Trigger PR builder.

--- cadvisor.go
+++ cadvisor.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// TODO(pwittrock): delete this comment.  Do not merge.
 package main
 
 import (
