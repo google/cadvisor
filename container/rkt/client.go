ebbda7e47f5a5db1763e75623c4f8d87e55c0b91
Shaya Potter <spotter@gmail.com>
2016-04-21 05:02:41 -0700

    add rkt api service version checking

--- container/rkt/client.go
+++ container/rkt/client.go
@@ -20,6 +20,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/coreos/go-semver/semver"
 	rktapi "github.com/coreos/rkt/api/v1alpha"
 
 	"golang.org/x/net/context"
@@ -27,6 +28,7 @@ import (
 )
 
 const (
+	minimumRktBinVersion     = "1.4.0"
 	defaultRktAPIServiceAddr = "localhost:15441"
 	timeout                  = 2 * time.Second
 )
@@ -37,25 +39,83 @@ var (
 	once         sync.Once
 )
 
+type rktVersion struct {
+	*semver.Version
+}
+
+func (r rktVersion) Compare(other string) (int, error) {
+	v, err := semver.NewVersion(other)
+	if err != nil {
+		return -1, err
+	}
+
+	if r.LessThan(*v) {
+		return -1, nil
+	}
+	if v.LessThan(*r.Version) {
+		return 1, nil
+	}
+	return 0, nil
+}
+
+func newRktVersion(version string) (rktVersion, error) {
+	sem, err := semver.NewVersion(version)
+	if err != nil {
+		return rktVersion{}, err
+	}
+	return rktVersion{sem}, nil
+}
+
 func Client() (rktapi.PublicAPIClient, error) {
 	once.Do(func() {
+		//gRPC doesn't fail fast if service isn't available so test with raw tcp connecction first
 		conn, err := net.DialTimeout("tcp", defaultRktAPIServiceAddr, timeout)
 		if err != nil {
 			rktClient = nil
 			rktClientErr = fmt.Errorf("rkt: cannot tcp Dial rkt api service: %v", err)
 			return
 		}
-
 		conn.Close()
 
+		rktClient = nil
 		apisvcConn, err := grpc.Dial(defaultRktAPIServiceAddr, grpc.WithInsecure(), grpc.WithTimeout(timeout))
 		if err != nil {
-			rktClient = nil
 			rktClientErr = fmt.Errorf("rkt: cannot grpc Dial rkt api service: %v", err)
 			return
 		}
 
-		rktClient = rktapi.NewPublicAPIClient(apisvcConn)
+		apisvc := rktapi.NewPublicAPIClient(apisvcConn)
+
+		resp, err := apisvc.GetInfo(context.Background(), &rktapi.GetInfoRequest{})
+		if err != nil {
+			rktClientErr = fmt.Errorf("rkt: GetInfo() failed: %v", err)
+			return
+		}
+
+		binVersion, err := newRktVersion(resp.Info.RktVersion)
+		if err != nil {
+			rktClientErr = fmt.Errorf("rkt: couldn't parse RtVersion: %v", err)
+			return
+		}
+		result, err := binVersion.Compare(minimumRktBinVersion)
+		if err != nil {
+			rktClientErr = fmt.Errorf("rkt: couldn't compare RktVersion against minimum: %v", err)
+			return
+		}
+		if result < 0 {
+			rktClientErr = fmt.Errorf("rkt: binary version is too old(%v), requires at least %v", resp.Info.RktVersion, minimumRktBinVersion)
+			return
+		}
+
+		//        	result, err = binVersion.Compare(recommendedRktBinVersion)
+		//	        if err != nil {
+		//        	        return err
+		//	        }
+		//        	if result != 0 {
+		//                	glog.Warningf("rkt: current binary version %q is not recommended (recommended version %q)", resp.Info.RktVersion, recommendedRktBinVersion)
+		//	        }
+
+		rktClient = apisvc
 	})
 
 	return rktClient, rktClientErr
