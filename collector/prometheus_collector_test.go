7a7ae080fd020510228f5606fa1c8144f5fa0485
Thomas Desrosiers <thomas@databricks.com>
2016-06-02 18:06:56 -0700

    Fixes a panic when a prometheus endpoint ends prematurely or with an empty line

--- collector/prometheus_collector_test.go
+++ collector/prometheus_collector_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/google/cadvisor/info/v1"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPrometheus(t *testing.T) {
@@ -44,13 +45,23 @@ func TestPrometheus(t *testing.T) {
 		text += "go_gc_duration_seconds{quantile=\"1\"} 0.000499764\n"
 		text += "# HELP go_goroutines Number of goroutines that currently exist.\n"
 		text += "# TYPE go_goroutines gauge\n"
-		text += "go_goroutines 16"
+		text += "go_goroutines 16\n"
+		text += "# HELP empty_metric A metric without any values\n"
+		text += "# TYPE empty_metric counter\n"
+		text += "\n"
 		fmt.Fprintln(w, text)
 	}))
 
 	defer tempServer.Close()
 
 	collector.configFile.Endpoint = tempServer.URL
+
+	var spec []v1.MetricSpec
+	require.NotPanics(t, func() { spec = collector.GetSpec() })
+	assert.Len(spec, 2)
+	assert.Equal(spec[0].Name, "go_gc_duration_seconds")
+	assert.Equal(spec[1].Name, "go_goroutines")
+
 	metrics := map[string][]v1.MetricVal{}
 	_, metrics, errMetric := collector.Collect(metrics)
 
@@ -62,6 +73,28 @@ func TestPrometheus(t *testing.T) {
 
 	goRoutines := metrics["go_goroutines"]
 	assert.Equal(goRoutines[0].FloatValue, 16)
+}
+
+func TestPrometheusShortResponse(t *testing.T) {
+	assert := assert.New(t)
+
+	//Create a prometheus collector using the config file 'sample_config_prometheus.json'
+	configFile, err := ioutil.ReadFile("config/sample_config_prometheus.json")
+	collector, err := NewPrometheusCollector("Prometheus", configFile, 100)
+	assert.NoError(err)
+	assert.Equal(collector.name, "Prometheus")
+	assert.Equal(collector.configFile.Endpoint, "http://localhost:8080/metrics")
+
+	tempServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		text := "# HELP empty_metric A metric without any values"
+		fmt.Fprint(w, text)
+	}))
+
+	defer tempServer.Close()
+
+	collector.configFile.Endpoint = tempServer.URL
+
+	assert.NotPanics(func() { collector.GetSpec() })
 }
 
 func TestPrometheusMetricCountLimit(t *testing.T) {
