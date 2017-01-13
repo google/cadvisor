98975a349d60d8a3d039a7d429c2b62e906ed9b0
Derek Carr <decarr@redhat.com>
2017-01-13 16:53:12 -0500

    Ignore any .mount cgroup in docker handler

--- container/docker/factory_test.go
+++ container/docker/factory_test.go
@@ -49,3 +49,24 @@ func TestEnsureThinLsKernelVersion(t *testing.T) {
 		}
 	}
 }
+
+func TestIsContainerName(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected bool
+	}{
+		{
+			name:     "/system.slice/var-lib-docker-overlay-9f086b233ab7c786bf8b40b164680b658a8f00e94323868e288d6ce20bc92193-merged.mount",
+			expected: false,
+		},
+		{
+			name:     "/system.slice/docker-72e5a5ff5eef3c4222a6551b992b9360a99122f77d2229783f0ee0946dfd800e.scope",
+			expected: true,
+		},
+	}
+	for _, test := range tests {
+		if actual := isContainerName(test.name); actual != test.expected {
+			t.Errorf("%s: expected: %v, actual: %v", test.name, test.expected, actual)
+		}
+	}
+}
