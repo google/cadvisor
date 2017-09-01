35de579b37e87b31932540f21f8e15249bb2c0c9
David Ashpole <dashpole@google.com>
2017-09-01 13:26:39 -0700

    update distribution

--- vendor/github.com/opencontainers/go-digest/verifiers.go
+++ vendor/github.com/opencontainers/go-digest/verifiers.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Docker, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package digest
 
 import (
@@ -15,19 +29,6 @@ type Verifier interface {
 	// Verified will return true if the content written to Verifier matches
 	// the digest.
 	Verified() bool
-}
-
-// NewDigestVerifier returns a verifier that compares the written bytes
-// against a passed in digest.
-func NewDigestVerifier(d Digest) (Verifier, error) {
-	if err := d.Validate(); err != nil {
-		return nil, err
-	}
-
-	return hashVerifier{
-		hash:   d.Algorithm().Hash(),
-		digest: d,
-	}, nil
 }
 
 type hashVerifier struct {
