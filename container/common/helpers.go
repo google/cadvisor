21dc5c3d7259e6f7a380c5aef4ee732400d4393f
Ted Yu <yuzhihong@gmail.com>
2020-02-24 11:25:59 -0800

    Assign quota if the parsing doesn't encounter error

--- container/common/helpers.go
+++ container/common/helpers.go
@@ -109,8 +109,9 @@ func GetSpec(cgroupPaths map[string]string, machineInfoFactory info.MachineInfoF
 				val, err := strconv.ParseUint(quota, 10, 64)
 				if err != nil {
 					klog.Errorf("GetSpec: Failed to parse CPUQuota from %q: %s", path.Join(cpuRoot, "cpu.cfs_quota_us"), err)
+				} else {
+					spec.Cpu.Quota = val
 				}
-				spec.Cpu.Quota = val
 			}
 		}
 	}
