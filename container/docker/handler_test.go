0460d1967681c7a6836ce3d239392715309a52cb
ChenQingya <qingya.chen520@gmail.com>
2020-03-20 11:36:02 +0800

    add unit test cases for raw method and new method matched with prefix specified

--- container/docker/handler_test.go
+++ container/docker/handler_test.go
@@ -19,8 +19,10 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
+	"github.com/docker/docker/api/types/container"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -46,5 +48,99 @@ func TestStorageDirDetectionWithNewVersions(t *testing.T) {
 	rwLayer, err = getRwLayerID(containerID, testDir, "aufs", []int{1, 10, 0})
 	as.Nil(err)
 	as.Equal(rwLayer, randomizedID)
+
+}
+
+func rawMetadataEnvMatch(dockerEnvWhiteList string, cntConfig container.Config) map[string]string {
+	metadataEnvs := strings.Split(dockerEnvWhiteList, ",")
+	handlerEnvs := make(map[string]string)
+
+	// split env vars to get metadata map.
+	for _, exposedEnv := range metadataEnvs {
+		for _, envVar := range cntConfig.Env {
+			if envVar != "" {
+				splits := strings.SplitN(envVar, "=", 2)
+				if len(splits) == 2 && splits[0] == exposedEnv {
+					handlerEnvs[strings.ToLower(exposedEnv)] = splits[1]
+				}
+			}
+		}
+	}
+
+	return handlerEnvs
+}
+
+func newMetadataEnvMatch(dockerEnvWhiteList string, cntConfig container.Config) map[string]string {
+	metadataEnvs := strings.Split(dockerEnvWhiteList, ",")
+	handlerEnvs := make(map[string]string)
+
+	// split env vars to get metadata map.
+	for _, exposedEnv := range metadataEnvs {
+		if exposedEnv == "" {
+			// if no dockerEnvWhitelist provided, len(metadataEnvs) == 1, metadataEnvs[0] == ""
+			continue
+		}
+
+		for _, envVar := range cntConfig.Env {
+			if envVar != "" {
+				splits := strings.SplitN(envVar, "=", 2)
+				if len(splits) == 2 && strings.HasPrefix(splits[0], exposedEnv) {
+					handlerEnvs[strings.ToLower(splits[0])] = splits[1]
+				}
+			}
+		}
+	}
+
+	return handlerEnvs
+}
+
+func TestDockerEnvWhitelist(t *testing.T) {
+	as := assert.New(t)
+
+	envTotalMatch := "TEST_REGION,TEST_ZONE"
+	envMatchWithPrefix := "TEST_"
+	envMatchWithPrefixEmpty := ""
+
+	rawCntConfig := container.Config{Env: []string{"TEST_REGION=FRA", "TEST_ZONE=A", "HELLO=WORLD"}}
+	newCntConfig := container.Config{Env: []string{"TEST_REGION=FRA", "TEST_ZONE=A", "TEST_POOL=TOOLING", "HELLO=WORLD"}}
+
+	rawExpected := map[string]string{
+		"test_region": "FRA",
+		"test_zone":   "A",
+	}
+	newExpected := map[string]string{
+		"test_region": "FRA",
+		"test_zone":   "A",
+		"test_pool":   "TOOLING",
+	}
+	emptyExpected := map[string]string{}
+
+	rawEnvsTotalMatch := rawMetadataEnvMatch(envTotalMatch, rawCntConfig)
+	newEnvsTotalMatch := newMetadataEnvMatch(envTotalMatch, rawCntConfig)
+
+	// make sure total match does not change
+	as.Equal(rawEnvsTotalMatch, newEnvsTotalMatch)
+	as.Equal(rawEnvsTotalMatch, rawExpected)
+
+	rawEnvsTotalMatch2 := rawMetadataEnvMatch(envTotalMatch, newCntConfig)
+	newEnvsTotalMatch2 := newMetadataEnvMatch(envTotalMatch, newCntConfig)
+
+	// make sure total match does not change with more envs exposed
+	as.Equal(rawEnvsTotalMatch2, newEnvsTotalMatch2)
+	as.Equal(rawEnvsTotalMatch2, rawExpected)
+
+	newEnvsMatchWithPrefix := newMetadataEnvMatch(envMatchWithPrefix, rawCntConfig)
+	newEnvsMatchWithPrefix2 := newMetadataEnvMatch(envMatchWithPrefix, newCntConfig)
+
+	// make sure new method can return envs with prefix specified
+	as.Equal(newEnvsMatchWithPrefix, rawExpected)
+	as.Equal(newEnvsMatchWithPrefix2, newExpected)
+
+	newEnvsMatchWithEmptyPrefix := newMetadataEnvMatch(envMatchWithPrefixEmpty, newCntConfig)
+	rawEnvsMatchWithEmptyWhitelist := rawMetadataEnvMatch(envMatchWithPrefixEmpty, newCntConfig)
+
+	// make sure empty whitelist returns nothing
+	as.Equal(newEnvsMatchWithEmptyPrefix, emptyExpected)
+	as.Equal(rawEnvsMatchWithEmptyWhitelist, emptyExpected)
 
 }
