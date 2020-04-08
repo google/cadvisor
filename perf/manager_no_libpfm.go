3f416b704af271699f20a85022d21791d53c4091
Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>
2020-04-08 09:35:15 +0200

    Little fixes to doc strings
    Signed-off-by: Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>

--- perf/manager_no_libpfm.go
+++ perf/manager_no_libpfm.go
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Manager of perf events for containers.
 package perf
 
 import (
--- perf/types.go
+++ perf/types.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Collector of perf events for a container.
-
 // Types related to handling perf events that are missing from unix package.
 package perf
 
