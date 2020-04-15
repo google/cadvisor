396c32a3b66908efd967d3855f4d1dd5b463bb3f
Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>
2020-04-15 08:31:40 +0200

    We no longer need to append entries to logfile
    Signed-off-by: Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>

--- build/integration.sh
+++ build/integration.sh
@@ -26,7 +26,7 @@ printf "" # Refresh sudo credentials if necessary.
 function start {
   set +e  # We want to handle errors if cAdvisor crashes.
   echo ">> starting cAdvisor locally"
-  GORACE="halt_on_error=1" ./cadvisor --docker_env_metadata_whitelist=TEST_VAR --v=6 --logtostderr $CADVISOR_ARGS &>> "$log_file"
+  GORACE="halt_on_error=1" ./cadvisor --docker_env_metadata_whitelist=TEST_VAR --v=6 --logtostderr $CADVISOR_ARGS &> "$log_file"
   if [ $? != 0 ]; then
     echo "!! cAdvisor exited unexpectedly with Exit $?"
     kill $TEST_PID # cAdvisor crashed: abort testing.
