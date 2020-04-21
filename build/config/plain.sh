0966731d893484a3fd7a3f0e5a758a271e1ed62e
Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>
2020-04-21 08:39:48 +0200

    Unsetting variables to reset environment
    Signed-off-by: Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>

--- build/config/plain.sh
+++ build/config/plain.sh
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Nothing happens here, this is just default config.
+unset GO_FLAGS
+unset PACKAGES
+unset BUILD_PACKAGES
+unset CADVISOR_ARGS
