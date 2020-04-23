f1cad012e255e0605f601539e540ec6a31804717
David Ashpole <dashpole@google.com>
2020-04-23 10:34:37 -0700

    new releases to cadvisor project

--- build/release.sh
+++ build/release.sh
@@ -41,7 +41,7 @@ build/build.sh
 # Build the docker image
 echo ">> building cadvisor docker image"
 docker_tag="google/cadvisor:$VERSION"
-gcr_tag="gcr.io/google_containers/cadvisor:$VERSION"
+gcr_tag="gcr.io/cadvisor/cadvisor:$VERSION"
 docker build -t $docker_tag -t $gcr_tag -f deploy/Dockerfile .
 
 echo
