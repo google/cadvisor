507ea187534bbfff473fa98fa9af90f85adee478
Maciej "Iwan" Iwanowski <maciej.iwanowski@critical.today>
2020-10-04 20:29:47 +0200

    BUILD_PACKAGED seems to be missing from env

--- build/config/libpfm4.sh
+++ build/config/libpfm4.sh
@@ -15,4 +15,5 @@
 export GO_FLAGS="-tags=libpfm,netgo -race"
 export PACKAGES="sudo libpfm4"
 export BUILD_PACKAGES="libpfm4 libpfm4-dev"
+export DEV_PACKAGES="libpfm4 libpfm4-dev"
 export CADVISOR_ARGS="-perf_events_config=perf/testing/perf-non-hardware.json"
