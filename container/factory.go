ded4f2f8758f840d7dbdc2f6fe4a2aa0963ed0ed
Eero Tamminen <eero.t.tamminen@intel.com>
2021-08-19 20:16:30 +0300

    Remove unused MetricSet.Append() method

--- container/factory.go
+++ container/factory.go
@@ -144,16 +144,6 @@ func (ms MetricSet) Difference(ms1 MetricSet) MetricSet {
 	return result
 }
 
-func (ms MetricSet) Append(ms1 MetricSet) MetricSet {
-	result := ms
-	for kind := range ms1 {
-		if !ms.Has(kind) {
-			result.Add(kind)
-		}
-	}
-	return result
-}
-
 // All registered auth provider plugins.
 var pluginsLock sync.Mutex
 var plugins = make(map[string]Plugin)
