21967e5f93211af9aa152683a29da862d7c409ca
Kir Kolyshkin <kolyshkin@gmail.com>
2021-10-20 18:45:28 -0700

    cmd: fix golint warning
    internal/container/install/install.go:19:2: a blank import should be only in a main or test package, or have a comment justifying it (golint)
    	_ "github.com/google/cadvisor/cmd/internal/container/mesos/install"
    	^
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- cmd/internal/container/install/install.go
+++ cmd/internal/container/install/install.go
@@ -16,6 +16,7 @@
 package install
 
 import (
+	// Register all included container providers.
 	_ "github.com/google/cadvisor/cmd/internal/container/mesos/install"
 	_ "github.com/google/cadvisor/container/containerd/install"
 	_ "github.com/google/cadvisor/container/crio/install"
