6d0fb8429aadc2777d9235d2d53eb1b04d6a95e6
Bartlomiej Plotka <bwplotka@gmail.com>
2021-10-22 17:20:53 +0200

    Added baseline benchmark for PrometheusCollector_Collect.
    ```
    /tmp/GoLand/___BenchmarkPrometheusCollector_Collect_in_github_com_google_cadvisor_metrics.test -test.v -test.paniconexit0 -test.bench ^\QBenchmarkPrometheusCollector_Collect\E$ -test.run ^$ -test.benchtime 10s
    goos: linux
    goarch: amd64
    pkg: github.com/google/cadvisor/metrics
    cpu: Intel(R) Core(TM) i7-9850H CPU @ 2.60GHz
    BenchmarkPrometheusCollector_Collect
    BenchmarkPrometheusCollector_Collect-12    	     748	  14929127 ns/op	 8253796 B/op	  207706 allocs/op
    PASS
    
    Process finished with the exit code 0
    ```
    
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

---
+++ metrics/prometheus_bench_test.go
@@ -0,0 +1,53 @@
+package metrics
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/cadvisor/container"
+	info "github.com/google/cadvisor/info/v1"
+	v2 "github.com/google/cadvisor/info/v2"
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/stretchr/testify/require"
+)
+
+var mfsTmp []*dto.MetricFamily
+
+type benchSubcontainersInfoProvider struct {
+	testSubcontainersInfoProvider
+
+	containers map[string]*info.ContainerInfo
+}
+
+func (p benchSubcontainersInfoProvider) GetRequestedContainersInfo(string, v2.RequestOptions) (map[string]*info.ContainerInfo, error) {
+	return p.containers, nil
+}
+
+func BenchmarkPrometheusCollector_Collect(b *testing.B) {
+	b.ReportAllocs()
+
+	// Generate bigger dataset for realistic situation (we can assume node has ~20 pods), which produces:
+	// * 84 metric families, with 5567 series in total.
+	// * 1081124 bytes (~1MB) of DTO struct.
+	// Reproducing https://github.com/kubernetes/kubernetes/issues/104459.
+	infoProvider := benchSubcontainersInfoProvider{containers: make(map[string]*info.ContainerInfo, 20)}
+	for i:=0; i<=20; i++ {
+		name := fmt.Sprintf("container-%v", i)
+		infoProvider.containers[name] = genContainerInfo(name)
+	}
+
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(NewPrometheusCollector(infoProvider, func(container *info.ContainerInfo) map[string]string {
+		s := DefaultContainerLabels(container)
+		s["zone.name"] = "hello"
+		return s
+	}, container.AllMetrics, now, v2.RequestOptions{}))
+
+	var err error
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mfsTmp, err = reg.Gather()
+		require.NoError(b, err)
+	}
+}
