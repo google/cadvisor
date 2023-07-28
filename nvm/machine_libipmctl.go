8953b04314967ba7d3c33effe0012377c402da15
Maciej "Iwan" Iwanowski <maciej.iwanowski@critical.today>
2023-07-28 18:44:52 +0200

    Information should be logged on increased verbosity only

--- nvm/machine_libipmctl.go
+++ nvm/machine_libipmctl.go
@@ -61,7 +61,7 @@ func getAvgPowerBudget() (uint, error) {
 	}
 
 	if count == 0 {
-		klog.V(1).Infof("There are no NVM devices.")
+		klog.V(4).Infof("There are no NVM devices.")
 		return uint(0), nil
 	}
 
