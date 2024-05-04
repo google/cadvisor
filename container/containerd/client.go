fbdac89d23ec0dba7e4b1b4112308271dcb823f0
Akhil Mohan <akhilerm@gmail.com>
2024-05-04 08:30:54 +0530

    use containerd/api module instead of third_party
    - use containerd/api v1.8.0-rc.2 instead of relying on third_party directory
    - remove script to copy /api from containerd/containerd
    
    Signed-off-by: Akhil Mohan <akhilerm@gmail.com>

--- container/containerd/client.go
+++ container/containerd/client.go
@@ -27,13 +27,13 @@ import (
 	"google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/credentials/insecure"
 
+	containersapi "github.com/containerd/containerd/api/services/containers/v1"
+	tasksapi "github.com/containerd/containerd/api/services/tasks/v1"
+	versionapi "github.com/containerd/containerd/api/services/version/v1"
+	tasktypes "github.com/containerd/containerd/api/types/task"
 	"github.com/google/cadvisor/container/containerd/containers"
 	"github.com/google/cadvisor/container/containerd/errdefs"
 	"github.com/google/cadvisor/container/containerd/pkg/dialer"
-	containersapi "github.com/google/cadvisor/third_party/containerd/api/services/containers/v1"
-	tasksapi "github.com/google/cadvisor/third_party/containerd/api/services/tasks/v1"
-	versionapi "github.com/google/cadvisor/third_party/containerd/api/services/version/v1"
-	tasktypes "github.com/google/cadvisor/third_party/containerd/api/types/task"
 )
 
 type client struct {
